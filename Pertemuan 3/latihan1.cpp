1d55258c603916e898e8d0147115a6fddee49b97
JhonPiterSitumorang-251401052 <jhonpiter2305@gmail.com>
2026-09-24 16:00:18 +0700

    Pertemuan 3 : Stack & Queue

---
+++ Pertemuan 3/latihan1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+// TODO : Menambahkan karakter ke stack
+void push(char value) {
+    if(top == MAX - 1){
+        cout << "Stack Penuh!\n";
+    } else {
+        top++;
+        stack[top] = value;
+        cout << value << " ditambahkan dalam stack\n";
+    }
+}
+
+// TODO : Mengambil karakter paling atas
+char pop() {
+    if(top == -1){
+        cout << "Stack kosong!\n";
+    } else {
+        cout << stack[top] << " ";
+        top--;
+    }
+}
+
+int main() {
+
+    string kata;
+
+    cout << "Masukkan sebuah kata: ";
+    cin >> kata;
+    cout << endl;
+
+    // TODO : Memasukkan setiap karakter ke stack
+    for (int i = 0; i < kata.length(); i++) {
+        push(kata[i]);
+    }
+
+    // TODO Mengeluarkan karakter dari stack
+    cout << endl;
+    while (top != -1) {
+        cout << pop();
+    }
+    
+    cout << endl;
+
+    return 0;
+}
